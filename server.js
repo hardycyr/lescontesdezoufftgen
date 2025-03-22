483252a4cfcaf35e1082fdc0071b8cc54905b10a
hardycyr <cyrille.hardy@gmail.com>
2025-03-22 09:22:27 +0100

    ping

--- server.js
+++ server.js
@@ -8,5 +8,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/ping", (req, res) => {
+    res.send("Pong! L'application est réveillée.");
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Serveur en ligne sur le port ${PORT}`));
