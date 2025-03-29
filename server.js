65002896f3e7653db91c6322e26028b8e4a4892d
hardycyr <cyrille.hardy@gmail.com>
2025-03-29 22:10:18 +0100

    s

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post("/create-checkout-session", async (req, res) => {
           shipping_address_collection: {
             allowed_countries: ["AC", "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AQ", "AR", "AT", "AU", "AW", "AX", "AZ",
                 "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV", "BW", "BY", "BZ",
-                "CA", "CC", "CD", "CF", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CY", "CZ",
+                "CA", "CD", "CF", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CY", "CZ",
                 "DE", "DJ", "DK", "DM", "DO", "DZ",
                 "EC", "EE", "EG", "EH", "ER", "ES", "ET",
                 "FI", "FJ", "FM", "FO", "FR",
